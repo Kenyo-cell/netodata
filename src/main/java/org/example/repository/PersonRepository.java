6597e8d8b45b8467eee4b636b8e63e7aa192fee1
Kenyo-cell <rekurt3000@gmail.com>
2022-04-05 23:28:49 +0300

    feat: jpql queries added

--- src/main/java/org/example/repository/PersonRepository.java
+++ src/main/java/org/example/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package org.example.repository;
 
 import org.example.entity.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +11,12 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-    List<PersonEntity> getAllByCityOfLiving(String city);
+    @Query("SELECT p FROM persons p WHERE p.cityOfLiving = :city")
+    List<PersonEntity> getAllByCityOfLiving(@Param("city") String city);
 
-    List<PersonEntity> getAllByAgeBeforeOrderByAgeAsc(int age);
+    @Query("SELECT p FROM persons p WHERE p.age < :age ORDER BY p.age")
+    List<PersonEntity> getAllByAgeBeforeOrderByAgeAsc(@Param("age") int age);
 
-    Optional<PersonEntity> getByNameAndSurname(String name, String surname);
+    @Query("SELECT p FROM persons p WHERE p.name = :name AND p.surname = :surname")
+    Optional<PersonEntity> getByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
